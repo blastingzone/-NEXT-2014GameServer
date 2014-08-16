bd68f63694e7a69498ce120c933daa8d4f9639c3
양현찬 <e7315126@naver.com>
2014-08-16 16:42:33 +0900

    5주차 숙제 commit

---
+++ HW_05_MT/EduServer_IOCP/PlayerManager.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "Player.h"
+#include "PlayerManager.h"
+
+PlayerManager* GPlayerManager = nullptr;
+
+PlayerManager::PlayerManager() : mLock(LO_ECONOMLY_CLASS), mCurrentIssueId(0)
+{
+
+}
+
+int PlayerManager::RegisterPlayer(std::shared_ptr<Player> player)
+{
+	FastSpinlockGuard exclusive(mLock);
+
+	mPlayerMap[++mCurrentIssueId] = player;
+
+	return mCurrentIssueId;
+}
+
+void PlayerManager::UnregisterPlayer(int playerId)
+{
+	FastSpinlockGuard exclusive(mLock);
+
+	mPlayerMap.erase(playerId);
+}
+
+
+int PlayerManager::GetCurrentPlayers(PlayerList& outList)
+{
+	//TODO: mLock을 read모드로 접근해서 outList에 현재 플레이어들의 정보를 담고 total에는 현재 플레이어의 총 수를 반환.
+	FastSpinlockGuard readLock(mLock, false);
+
+	int total = 0;
+	for (auto iter : mPlayerMap)
+	{
+		outList.push_back(iter.second);
+		++total;
+	}
+
+	return total;
+}
