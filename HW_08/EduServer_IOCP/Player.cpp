9d41db905fb3e52ccee0a15b8d0415ceb85e610f
양현찬 <e7315126@naver.com>
2014-09-14 17:16:23 +0900

    SQL문 추가

--- HW_08/EduServer_IOCP/Player.cpp
+++ HW_08/EduServer_IOCP/Player.cpp
@@ -55,17 +55,6 @@ void Player::OnTick()
 	if (!IsAlive())
 		return;
 
-	
-	/// 랜덤으로 이벤트를 발생시켜보기 (예: 다른 모든 플레이어에게 버프 주기)
-	//이걸 늘리면 버그를 찾을 수 있을 듯하군
-	//if (rand() % 100 == 0) ///< 1% 확률
-	//가끔 LockOrder에서 뻑이남 처음에 몇번나고 안나서 짜증남
-	//tick에 있는 lock에서 뻑이남
-	//덤프파일 남기기 성공 LockOrder의 pop순서에서 뻑이남
-	//timer에서 owner의 lock을 사용 task실행후 어 leavelock하니 이전에 걸었던 락하고 다르네?
-	//1.task실행과정 중에 뭔가가 일어났다.
-	//2.다른 쓰레드에서 해당 owner의 lock을 건드렸다.
-	//
 	if (true)
 	{
 		int buffId = mPlayerId * 100;
@@ -76,15 +65,6 @@ void Player::OnTick()
 		auto playerEvent = std::make_shared<AllPlayerBuffEvent>(buffId, duration);
 		GCEDispatch(playerEvent, &AllPlayerBuffEvent::DoBuffToAllPlayers, mPlayerId);
 	}
-
-
-	//TODO: AllPlayerBuffDecay::CheckBuffTimeout를 GrandCentralExecuter를 통해 실행
-	//make_shared이거 메모리 할당 아닌가? 그럼 풀에서 받아와야하는 것 아닌가?
-	//뜯어보니 stl에서 만들어진 객체(_Ref_count_obj)에 할당하고 있는 것 같다.
-	//allocate_shared를 이용하면 가능하다고 한다.(make_shared와 같은 기능을 하는 것 같다)
-	//make_shared 하나의 메모리 블럭안에 포인터와 객체를 담는다.
-
-	///# 그래 정석대로라면 풀에서 받는게 맞지..
 
 	auto playerDecay = std::make_shared<AllPlayerBuffDecay>();
 	GCEDispatch(playerDecay, &AllPlayerBuffDecay::CheckBuffTimeout);
