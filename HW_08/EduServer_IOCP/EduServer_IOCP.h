39bfe44d4af17e78d1cca0d155dee77ff9c45ccf
1기 문진상 <blastingzone@naver.com>
2014-09-21 21:12:48 +0900

    계정변경

--- HW_08/EduServer_IOCP/EduServer_IOCP.h
+++ HW_08/EduServer_IOCP/EduServer_IOCP.h
@@ -6,8 +6,8 @@
 #define CONNECT_SERVER_ADDR	"127.0.0.1"
 #define CONNECT_SERVER_PORT 9001
 
-#define SQL_SERVER_CONN_STR	L"Driver={SQL Server};Server=BLASTINGZONE-PC\\SQLEXPRESS;Database=NGServer;UID=next;PWD=1234;"
-//#define SQL_SERVER_CONN_STR	L"Driver={SQL Server};Server=127.0.0.1,1433\\YANGHYUNCHAN-PC;Database=GameDB;UID=justdid;PWD=1234"
+//#define SQL_SERVER_CONN_STR	L"Driver={SQL Server};Server=BLASTINGZONE-PC\\SQLEXPRESS;Database=NGServer;UID=next;PWD=1234;"
+#define SQL_SERVER_CONN_STR	L"Driver={SQL Server};Server=127.0.0.1,1433\\YANGHYUNCHAN-PC;Database=GameDB;UID=justdid;PWD=1234"
 
 //#define GQCS_TIMEOUT	10 //INFINITE
 #define GQCS_TIMEOUT	INFINITE //20
