dc13201bd5ab53febce17263a04958b9486d0ca1
양현찬 <e7315126@naver.com>
2014-09-24 17:07:04 +0900

    폴더정리 SpinLock추가

--- HW_08/IOCP_Client/stdafx.h
+++ HW_08/IOCP_Client/stdafx.h
@@ -1,1 +1,19 @@
-//Ghost File
+
+#pragma once
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <process.h>
+#include <assert.h>
+#include <limits.h>
+
+#include <WinSock2.h>
+#include <Mswsock.h>
+
+#include <cstdint>
+#include <memory>
+#include <deque>
+#include <vector>
+#include <type_traits>
+#include <algorithm>
